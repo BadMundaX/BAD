2a26b47e9e4a05aa20bf5af4a3480d4eb71c8409
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-09 20:55:10 +0530

    Update start.py

--- BADMUSIC/plugins/bot/start.py
+++ BADMUSIC/plugins/bot/start.py
@@ -18,7 +18,7 @@ from youtubesearchpython.__future__ import VideosSearch
 import config
 from config import BANNED_USERS, START_IMG_URL
 from strings import get_string
-from BADMUSIC import Platform, app
+from BADMUSIC import *
 from BADMUSIC.misc import SUDOERS, _boot_
 from BADMUSIC.plugins.sudo.sudoers import sudoers_list
 from BADMUSIC.utils.database import (
@@ -145,7 +145,7 @@ async def start_comm(client, message: Message, _):
             except Exception as e:
                 print(e)
                 return
-            thumbnail = await Platform.YouTube.thumbnail(videoid, True)
+            thumbnail = await YouTube.thumbnail(videoid, True)
             await m.delete()
             await message.reply_photo(photo=thumbnail, caption=msg)
             return
@@ -166,7 +166,7 @@ async def start_comm(client, message: Message, _):
             lyrical = config.lyrical
             lyrics = lyrical.get(query)
             if lyrics:
-                await Platform.Telegram.send_split_text(message, lyrics)
+                await Telegram.send_split_text(message, lyrics)
                 return
             else:
                 await message.reply_text("ғᴀɪʟᴇᴅ ᴛᴏ ɢᴇᴛ ʟʏʀɪᴄs.")
